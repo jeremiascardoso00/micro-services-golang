b4beb2b24bafcf04d494f410ce45f04bdccd032c
jeremiascardoso00 <jeremiascardoso00@gmail.com>
2024-11-24 19:14:01 -0300

    initial commit

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	err := app.writeJSON(w, http.StatusOK, payload)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusUnprocessableEntity)
+	}
+
+}
